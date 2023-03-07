66b2aa8c57ee57c4858a5ead91bffefb7815506f
sumant4 <sumant@lmnl.app>
2023-03-07 16:04:26 +0530

    Added Rupee Footer Issue

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ import { AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai';
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p>2022 Hot Deals All rights reserverd</p>
+      <p>2022 &#169; Hot Deals &#174; All Rights Reserverd </p>
       <p className="icons">
         <AiFillInstagram />
         <AiOutlineTwitter />
